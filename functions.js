94d4004030155aae4c3a7f76152b9b8d7711e8f8
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-08-13 14:02:09 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,9 @@
+cursor = document.querySelector(".cursor")
+
+document.addEventListener("mousemove", function(e) {
+    var x = e.clientX
+    var y = e.clientY
+    cursor.style.left = x + "px"
+    cursor.style.top = y + "px"
+
+})
