55971c9151f00107c626fd974a53c742def405b5
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-08-19 16:03:46 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,9 +1,38 @@
-cursor = document.querySelector(".cursor")
+const mouse = document.querySelector(".mouse");
 
-document.addEventListener("mousemove", function(e) {
-    var x = e.clientX
-    var y = e.clientY
-    cursor.style.left = x + "px"
-    cursor.style.top = y + "px"
+function isTouchDevice() {
+    try {
+        document.createEvent("TouchEvent");
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
+}
 
+const move = (e) => {
+    try {
+        var x = !isTouchDevice() ? e.pageX : e.touches
+        [0].pageX;
+        var y = !isTouchDevice() ? e.pageY : e.touches
+        [0].pageY;
+    }
+    catch (e) {}
+    mouse.style.left = x + "px"
+    mouse.style.top = y + "px"
+
+    
+}
+
+document.addEventListener("mousemove", (e) => {
+    if (document.body.style.cursor === "pointer") {
+        console.log("hi")
+        document.body.style.cursor = "normal"
+        mouse.style.height = "25px"
+        mouse.style.width = "25px"
+    }
+    move(e)
 })
+
+
+document.querySelector(".splineBG").style.cursor = "none";
